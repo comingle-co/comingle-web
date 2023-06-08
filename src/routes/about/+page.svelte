4884de6ddca5fb0220b19b46d8a656ba43338fe9
Terry Yiu <963907+tyiu@users.noreply.github.com>
2023-06-07 21:06:45 -0400

    Change prettier config to use spaces instead of tabs

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,27 +1,40 @@
 <svelte:head>
-	<title>About - Confstr</title>
-	<meta name="description" content="About this app" />
+  <title>About - Confstr</title>
+  <meta name="description" content="About this app" />
 </svelte:head>
 
 <div class="text-column">
-	<h1>About this app</h1>
+  <h1>About this app</h1>
 
-	<p>
-		Confstr is a conference app powered by <a href="https://github.com/nostr-protocol/nostr">Nostr</a>. It is currently under development. You can follow development on <a href=https://github.com/confstr-app>GitHub</a>.
-	</p>
+  <p>
+    Confstr is a conference app powered by <a href="https://github.com/nostr-protocol/nostr"
+      >Nostr</a
+    >. It is currently under development. You can follow development on
+    <a href="https://github.com/confstr-app">GitHub</a>.
+  </p>
 
-	<p>
-		Goals
-	</p>
+  <p>Goals</p>
 
-	<ul>
-		<li>Build clients (iOS, Android, Web) for conference attendees to view conferences, schedules, sessions, speakers</li>
-		<li>Provide a way for conference attendees to bookmark sessions</li>
-		<li>Allow mobile clients to view conference data even when offline</li>
-		<li>Provide a way for conference organizers to add and modify conference data from the web that is read by the attendee mobile apps</li>
-		<li>Internationalize the app, website, and conference data so that it will be possible to localize to any locale</li>
-		<li>Generalize this solution to work for any conference</li>
-		<li>Decentralize the source of conference data using Nostr</li>
-		<li>Provide value-for-value to conference speakers and organizers through Bitcoin Lightning network</li>
-	</ul>
+  <ul>
+    <li>
+      Build clients (iOS, Android, Web) for conference attendees to view conferences, schedules,
+      sessions, speakers
+    </li>
+    <li>Provide a way for conference attendees to bookmark sessions</li>
+    <li>Allow mobile clients to view conference data even when offline</li>
+    <li>
+      Provide a way for conference organizers to add and modify conference data from the web that is
+      read by the attendee mobile apps
+    </li>
+    <li>
+      Internationalize the app, website, and conference data so that it will be possible to localize
+      to any locale
+    </li>
+    <li>Generalize this solution to work for any conference</li>
+    <li>Decentralize the source of conference data using Nostr</li>
+    <li>
+      Provide value-for-value to conference speakers and organizers through Bitcoin Lightning
+      network
+    </li>
+  </ul>
 </div>
